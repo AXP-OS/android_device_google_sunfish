3f2b3206d60706fffb888036c1cf800b7fc1a92e
AXP.OS <sherlock@binbash.rocks>
2025-06-26 10:27:40 +0200

    sunfish: remove reserved space for AXP.OS

--- device-lineage.mk
+++ device-lineage.mk
@@ -18,8 +18,6 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
-    ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
 
 # EUICC
 PRODUCT_COPY_FILES += \
@@ -41,12 +39,9 @@ PRODUCT_PACKAGES += \
 
 # Parts
 PRODUCT_PACKAGES += \
-    GoogleParts
 
 # RCS
 PRODUCT_PACKAGES += \
-    PresencePolling \
-    RcsService
 
 # Touch
 include hardware/google/pixel/touch/device.mk
